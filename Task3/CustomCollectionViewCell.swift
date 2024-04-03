66c9bdbea1748067f55e78051b9d1f7dc420bfad
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-04-03 21:30:24 +0530

    First Commit

---
+++ Task3/CustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewCell.swift
+//  Task3
+//
+//  Created by manvendu pathak  on 01/04/24.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
